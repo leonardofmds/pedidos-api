921abe8828ce8877b4a62ccf3e16854840b06c58
Leonardo Menezes <leonardomenezes.dev@gmail.com>
2025-06-12 09:13:20 -0300

    Create PedidosApiApplicationTests.java

---
+++ src/test/java/br/com/cotiinformatica/PedidosApiApplicationTests.java
@@ -0,0 +1,30 @@
+package br.com.cotiinformatica;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosApiApplicationTests {
+
+	@Test
+	public void deveCriarPedidoComSucesso() throws Exception {
+		fail("Não implementado.");
+	}
+
+	@Test
+	public void deveAtualizarPedidoComSucesso() throws Exception {
+		fail("Não implementado.");
+	}
+
+	@Test
+	public void deveExcluirPedidoComSucesso() throws Exception {
+		fail("Não implementado.");
+	}
+
+	@Test
+	public void deveConsultarPedidosComSucesso() throws Exception {
+		fail("Não implementado.");
+	}
+}
