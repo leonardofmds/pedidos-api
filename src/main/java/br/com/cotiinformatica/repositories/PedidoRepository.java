1c6d9551baefbb953ed6a836a980141eb9cc1014
Leonardo Menezes <leonardomenezes.dev@gmail.com>
2025-06-12 09:14:44 -0300

    Create PedidoRepository.java

---
+++ src/main/java/br/com/cotiinformatica/repositories/PedidoRepository.java
@@ -0,0 +1,13 @@
+package br.com.cotiinformatica.repositories;
+
+import br.com.cotiinformatica.entities.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, UUID> {
+
+
+}
